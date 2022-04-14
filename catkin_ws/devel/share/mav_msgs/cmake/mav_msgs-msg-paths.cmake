f281126fd98fab747cba3933c3a71bd58650af11
leeandy0822 <leeandy90833@gmail.com>
2022-04-14 14:56:25 +0800

    finish challenge1, 2

--- catkin_ws/devel/share/mav_msgs/cmake/mav_msgs-msg-paths.cmake
+++ catkin_ws/devel/share/mav_msgs/cmake/mav_msgs-msg-paths.cmake
@@ -1,4 +1,4 @@
 # generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
 
-set(mav_msgs_MSG_INCLUDE_DIRS "/home/ncrl/ncrl_robotics/catkin_ws/src/finalproject/mav_comm/mav_msgs/msg")
+set(mav_msgs_MSG_INCLUDE_DIRS "/home/ncrl/ncrl_robotics/catkin_ws/src/finalproject/multirotor_geometry_control/mav_comm/mav_msgs/msg")
 set(mav_msgs_MSG_DEPENDENCIES std_msgs;geometry_msgs)
--- catkin_ws/devel/share/rotors_comm/cmake/rotors_comm-msg-extras.cmake
+++ catkin_ws/devel/share/rotors_comm/cmake/rotors_comm-msg-extras.cmake
@@ -1,2 +1,2 @@
-set(rotors_comm_MESSAGE_FILES "/home/ncrl/ncrl_robotics/catkin_ws/src/finalproject/rotors_simulator/rotors_comm/msg/WindSpeed.msg")
-set(rotors_comm_SERVICE_FILES "/home/ncrl/ncrl_robotics/catkin_ws/src/finalproject/rotors_simulator/rotors_comm/srv/Octomap.srv;/home/ncrl/ncrl_robotics/catkin_ws/src/finalproject/rotors_simulator/rotors_comm/srv/RecordRosbag.srv")
+set(rotors_comm_MESSAGE_FILES "/home/ncrl/ncrl_robotics/catkin_ws/src/finalproject/multirotor_geometry_control/rotors_simulator/rotors_comm/msg/WindSpeed.msg")
+set(rotors_comm_SERVICE_FILES "/home/ncrl/ncrl_robotics/catkin_ws/src/finalproject/multirotor_geometry_control/rotors_simulator/rotors_comm/srv/Octomap.srv;/home/ncrl/ncrl_robotics/catkin_ws/src/finalproject/multirotor_geometry_control/rotors_simulator/rotors_comm/srv/RecordRosbag.srv")
